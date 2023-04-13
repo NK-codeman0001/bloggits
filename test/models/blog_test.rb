2d83026ad74f091323df751d7f90f4ac809b4163
Neeraj Kumar <neeraj.kumar@berylsystems.com>
2023-04-13 19:02:41 +0530

    Implements 9 tests for blog model

--- test/models/blog_test.rb
+++ test/models/blog_test.rb
@@ -4,4 +4,46 @@ class BlogTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "draft? returns true for draft blog" do
+    assert blogs(:draft).draft?
+  end
+
+  test "draft? returns false for published blog" do
+    refute blogs(:published).draft?
+  end
+
+  test "draft? returns false for scheduled blog" do
+    refute blogs(:scheduled).draft?
+  end
+
+
+
+  test "published? returns true for published blog" do
+    assert blogs(:published).published?
+  end
+
+  test "published? returns false for draft blog" do
+    refute blogs(:draft).published?
+  end
+
+  test "published? returns false for scheduled blog" do
+    refute blogs(:scheduled).published?
+  end
+
+  
+  
+  test "scheduled? returns true for scheduled? blog" do
+    assert blogs(:scheduled).scheduled?
+  end
+
+  test "scheduled? returns false for published blog" do
+    refute blogs(:published).scheduled?
+  end
+
+  test "scheduled? returns false for draft blog" do
+    refute blogs(:draft).scheduled?
+  end
+
+  
 end
