c21b8b1fd1246672189aa9b1af73d534fb2c8a01
Neeraj Kumar <neeraj.kumar@berylsystems.com>
2023-04-14 18:25:55 +0530

    add 100 demo blogs in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,3 +12,12 @@ adminuser.update(
   password: "changeit",
   password_confirmation: "changeit"
 )
+
+
+100.times do |i|
+  blog = Blog.where(title: "Demo Blog: #{i+1}").first_or_initialize
+  blog.update(
+    content: "This is seeded content no- #{i+1}",
+    published_at: Time.current
+  )
+end
