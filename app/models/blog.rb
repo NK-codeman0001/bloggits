0e952fd78dbb83f0d8463cbc8487f6e25d60dc02
Neeraj Kumar <neeraj.kumar@berylsystems.com>
2023-04-14 17:34:31 +0530

    modify blog validations

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,9 +1,9 @@
 class Blog < ApplicationRecord
   has_rich_text :content
 
-  validates :title, presence: true, length: {maximum: 60}
-  validates :body, presence: true, length: {maximum: 255}
-  validates :published_at, comparison: {greater_than_or_equal_to: Time.current, message: "can't be in past"}, allow_nil: true
+  validates :title, presence: true, length: {maximum: 120}
+  validates :content, presence: true
+  # validates :published_at, comparison: {greater_than_or_equal_to: Time.current, message: "can't be in past"}, allow_nil: true
   
   scope :draft, -> { where(published_at: nil)}
   scope :published, -> { where("published_at <= ?", Time.current) }
