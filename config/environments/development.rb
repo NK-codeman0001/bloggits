e06bcf5e8944d0a6205b7f97a56446da20d4f2f1
Neeraj Kumar <neeraj.kumar@berylsystems.com>
2023-04-20 15:42:40 +0530

    add smtp config for action mailer using Elastic Mail SMTP

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -69,5 +69,18 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = {from: 'neeraj.kumar@berylsystems.com'}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.elasticemail.com",
+    port: 2525,
+    domain: "bloggits.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: "neeraj.kumar@berylsystems.com",
+    password: "21DF69B603096C45B23456A4C43CFE45EC45"
+  }
 
 end
