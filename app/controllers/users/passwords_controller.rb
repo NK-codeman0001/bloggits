5359260ceaabaaf019b0e3ea707171de20fdf490
Neeraj Kumar <neeraj.kumar@berylsystems.com>
2023-04-21 14:19:22 +0530

    add user devise controllers

---
+++ app/controllers/users/passwords_controller.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+class Users::PasswordsController < Devise::PasswordsController
+  # GET /resource/password/new
+  # def new
+  #   super
+  # end
+
+  # POST /resource/password
+  # def create
+  #   super
+  # end
+
+  # GET /resource/password/edit?reset_password_token=abcdef
+  # def edit
+  #   super
+  # end
+
+  # PUT /resource/password
+  # def update
+  #   super do |resource|
+  #     if resource.errors.empty?
+  #       # Redirect to the login page after updating the password
+  #       destroy_user_session(resource)
+  #       redirect_to new_session_path(resource) and return
+  #     end
+  #   end
+  # end
+
+  # protected
+
+  # def after_resetting_password_path_for(resource)
+  #   super(resource)
+  # end
+
+  # The path used after sending reset password instructions
+  # def after_sending_reset_password_instructions_path_for(resource_name)
+  #   super(resource_name)
+  # end
+end
