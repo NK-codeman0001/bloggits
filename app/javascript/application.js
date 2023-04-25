aa89b57ebfb51debee8100726d7ac5f11114229f
Neeraj Kumar <neeraj.kumar@berylsystems.com>
2023-04-25 11:11:19 +0530

    add archive-blogs button with jquery code to hide/show button

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,3 +3,21 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import "trix"
 import "@rails/actiontext"
+
+$(document).ready(function(){
+  var counter = 0;
+  $('.book-select-check').on('click',function(){
+    if($(this).prop('checked')){
+      counter +=1;
+      //$('#archive-blogs').css('display','block');
+      $('#archive-blogs').show();
+    }
+    else {
+      counter -=1;
+      if(counter <=0){
+    //$('#archive-blogs').css('display','none');
+      $('#archive-blogs').hide();
+      }
+    }
+  });
+});
