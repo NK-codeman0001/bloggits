22bbd29665069ba3b7622977aa21319b6e507687
Neeraj Kumar <neeraj.kumar@berylsystems.com>
2023-04-28 14:23:41 +0530

    Resolve Issue: archive and select all is not working after search

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,12 +3,7 @@ import "@hotwired/turbo-rails";
 import "controllers";
 import "trix";
 import "@rails/actiontext";
-//= require jquery
-//= require jquery.turbolinks
-//= require_tree .
-
-var ready;
-ready = function () {
+function setupCheckboxes() {
   var counter = 0;
   $(".blog-select-check").on("click", function () {
     if ($(this).prop("checked")) {
@@ -52,6 +47,13 @@ ready = function () {
       });
     }
   });
+}
+
+
+var ready;
+ready = function () {
+  setupCheckboxes();
+
   $("#search_field").on("keyup", function () {
     var searchKey = $("#search_field").val();
 
@@ -59,6 +61,9 @@ ready = function () {
       url: "blogs/search",
       type: "GET",
       data: { search: searchKey },
+      success: function() {
+        setupCheckboxes();
+      }
     });
   });
 };
@@ -66,51 +71,3 @@ ready = function () {
 // $(document).ready(ready);
 $(document).on("turbo:load", ready);
 
-// // $(document).ready(function(){
-// $(document).on('turbolinks:load', function() {
-// // document.addEventListener("turbolinks:load", function() {
-
-//   var counter = 0;
-//   $('.blog-select-check').on('click',function(){
-//     if($(this).prop('checked')){
-//       counter +=1;
-//     }
-//     else {
-//       counter -=1;
-//     }
-//   });
-
-//   $('#select-all-blog').on('click', function(){
-//     $('.blog-select-check').prop('checked', $(this). prop('checked'));
-//   });
-
-//   $('#archive-blogs').on('click', function(){
-//     if(counter <=0){
-//         alert("Please select atleast one blog");
-//         }
-//         else {
-//     var blogIds = [];
-//     $('.blog-select-check').each(function(){
-//       if($(this).prop('checked')){
-//         blogIds.push($(this).data('blog-id'))
-//       }
-
-//     });
-
-//     $.ajax({
-//       url: 'blogs/bulk_archive_blogs',
-//       type: 'PATCH',
-//       data: {blog_ids: blogIds}
-//     });
-//   }
-//   });
-//   $("#search_field" ).on( "keyup", function() {
-//     var searchKey = $("#search_field").val();
-
-//     $.ajax({
-//       url: 'blogs/search',
-//       type: 'GET',
-//       data: {search: searchKey}
-//     });
-//   });
-// });
