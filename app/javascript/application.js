93b77d9f8d768bc9eca96752db48bf163c6f3d12
Neeraj Kumar <neeraj.kumar@berylsystems.com>
2023-04-28 10:28:33 +0530

    Resolve: Alert Not showing when archive is pressed 2nd time after archiveing blogs

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,78 +1,70 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
-import "trix"
-import "@rails/actiontext"
+import "@hotwired/turbo-rails";
+import "controllers";
+import "trix";
+import "@rails/actiontext";
 //= require jquery
 //= require jquery.turbolinks
 //= require_tree .
 
+var ready;
+ready = function () {
+  var counter = 0;
+  $(".blog-select-check").on("click", function () {
+    if ($(this).prop("checked")) {
+      counter += 1;
+    } else {
+      counter -= 1;
+      $("#select-all-blogs").prop("checked", false);
+    }
+  });
 
+  $("#select-all-blogs").on("click", function () {
+    if ($(this).prop("checked")) {
+      $(".blog-select-check").each(function () {
+        $(this).prop("checked", true);
+        counter += 1;
+      });
+    } else {
+      $(".blog-select-check").each(function () {
+        $(this).prop("checked", false);
+        counter -= 1;
+      });
+    }
+  });
 
- var ready;
- ready = function() {
-      var counter = 0;
-      $('.blog-select-check').on('click',function(){
-        if($(this).prop('checked')){
-          counter +=1;
-        }
-        else {
-          counter -=1;
-          $('#select-all-blogs').prop('checked', false);
+  $("#archive-blogs").on("click", function () {
+    if (counter <= 0) {
+      alert("Please select atleast one blog");
+    } else {
+      var blogIds = [];
+      $(".blog-select-check").each(function () {
+        if ($(this).prop("checked")) {
+          blogIds.push($(this).data("blog-id"));
         }
       });
-    
-      $('#select-all-blogs').on('click', function(){
-        if($(this).prop('checked')){
-        $('.blog-select-check').each(function() {
-          $(this).prop('checked', true);
-          counter +=1;
-        });
-        }
-        else{
-          $('.blog-select-check').each(function() {
-            $(this).prop('checked', false);
-            counter -=1;  
-          });
+      counter = 0;
 
-        }
+      $.ajax({
+        url: "blogs/bulk_archive_blogs",
+        type: "PATCH",
+        data: { blog_ids: blogIds },
+      });
+    }
+  });
+  $("#search_field").on("keyup", function () {
+    var searchKey = $("#search_field").val();
 
-      });
-    
-      $('#archive-blogs').on('click', function(){
-        if(counter <=0){
-            alert("Please select atleast one blog");
-            }
-            else {
-        var blogIds = [];
-        $('.blog-select-check').each(function(){
-          if($(this).prop('checked')){
-            blogIds.push($(this).data('blog-id'))
-          }
-        
-        });
-    
-        $.ajax({
-          url: 'blogs/bulk_archive_blogs',
-          type: 'PATCH',
-          data: {blog_ids: blogIds}
-        });
-      }
-      });
-      $("#search_field" ).on( "keyup", function() {
-        var searchKey = $("#search_field").val();
-        
-    
-        $.ajax({
-          url: 'blogs/search',
-          type: 'GET',
-          data: {search: searchKey}
-        });
-      });
-    };
+    $.ajax({
+      url: "blogs/search",
+      type: "GET",
+      data: { search: searchKey },
+    });
+  });
+};
 
-  // $(document).ready(ready);
-  $(document).on('turbo:load', ready);    
+// $(document).ready(ready);
+$(document).on("turbo:load", ready);
 
 // // $(document).ready(function(){
 // $(document).on('turbolinks:load', function() {
@@ -102,7 +94,7 @@ import "@rails/actiontext"
 //       if($(this).prop('checked')){
 //         blogIds.push($(this).data('blog-id'))
 //       }
-    
+
 //     });
 
 //     $.ajax({
@@ -114,7 +106,6 @@ import "@rails/actiontext"
 //   });
 //   $("#search_field" ).on( "keyup", function() {
 //     var searchKey = $("#search_field").val();
-    
 
 //     $.ajax({
 //       url: 'blogs/search',
