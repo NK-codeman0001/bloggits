1141365513df991579e0bffb3f5836bc7c94f28d
Neerj Kumar <neeraj.kumar@berylsystems.com>
2023-05-12 12:14:30 +0000

    commit to rebase from server

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -9,3 +9,4 @@ stdout_path "/home/ubuntu/bloggits/log/unicorn.log"
 listen "/tmp/unicorn.bloggits.sock"
 worker_processes 2
 timeout 38
+
--- db/migrate/20230424105936_add_column_to_users.rb
+++ db/migrate/20230424105936_add_column_to_users.rb
@@ -1,7 +1,5 @@
 class AddColumnToUsers < ActiveRecord::Migration[7.0]
   def change
-    remove_column :users, :provider, :string
-    remove_column :users, :uid, :string
     add_column :users, :provider, :string
     add_column :users, :uid, :string
   end
